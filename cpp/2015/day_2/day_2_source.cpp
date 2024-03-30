059359bf945de68d518e537799d159137b486342
coding-snake <tabiszewski.damian.008@gmail.com>
2024-03-30 13:16:19 +0100

    commit

--- cpp/2015/day_2/day_2_source.cpp
+++ cpp/2015/day_2/day_2_source.cpp
@@ -1,92 +1,77 @@
 #include "console_tools.h"
 #include "arr_tools.h"
-#include "Math_tools.h"
+
 #include <iostream>
 #include <fstream>
-
+#include <algorithm>
+#include <cmath>
 
 int main()
 {
-	// opening file
-	std::ifstream file;
+    // opening file
+    std::ifstream file;
 
-	file.open("day_2_input.txt");
-	if (!file.is_open())
-	{
-		std::cerr << "Failed to open a file\n";
-		return 1;
-	}
+    file.open("day_2_input.txt");
+    if (!file.is_open())
+    {
+        std::cerr << "Failed to open a file\n";
+        return 1;
+    }
 
+    // core loop
+    constexpr int size{50};
+    char line[size]{};
 
-	// core loop
-	constexpr int size{ 50 };
-	char line[size]{};
+    int paper{0};
+    int ribbon{0};
 
-	int paper{ 0 };
-	int ribbon{ 0 };
+    while (file.getline(line, size))
+    {
+        // getting first value
+        int left{0};
+        int right{0};
 
-	while (file.getline(line, size))
-	{
-		int left{ 0 };
-		int right{ 0 };
+        right = {Arr_tools::index_of_delimeter(line, right, 'x', size) - 1};
 
-		// search for index of delimeter and setting up value
+        int x{Arr_tools::convertSubarrayToInt(line, left, right)};
 
-		right = { Arr_tools::index_of_delimeter(line, right, 'x', size) - 1 };
+        // getting second value
+        right += 2;
+        left = {right};
 
-		int x{ Arr_tools::convertSubarrayToInt(line, left, right) };
+        right = {Arr_tools::index_of_delimeter(line, right, 'x', size) - 1};
 
-		// adjustment
-		right += 2;
-		left = { right };
+        int y{Arr_tools::convertSubarrayToInt(line, left, right)};
 
-		// search for index of delimeter and setting up value
+        // getting third value
+        right += 2;
+        left = {right};
 
-		right = { Arr_tools::index_of_delimeter(line, right, 'x', size) - 1 };
+        right = {Arr_tools::index_of_delimeter(line, right, '\0', size) - 1};
 
-		int y{ Arr_tools::convertSubarrayToInt(line, left, right) };
+        int z{Arr_tools::convertSubarrayToInt(line, left, right)};
 
-		// adjustment
-		right += 2;
-		left = { right };
+        // caluclating 2 smallest values
+        int min1{std::min(std::min(x, y), z)};
+        int min2{(x + y + z) - min1 - std::max(std::max(x, y), z)};
 
-		// search for index of delimeter and setting up value
+        // calculating paper
+        paper += 2 * ((x * y) + (y * z) + (x * z));
+        paper += min1 * min2;
 
-		right = { Arr_tools::index_of_delimeter(line, right, '\0', size) - 1 };
+        // calculating ribbon
+        ribbon += (2 * min1) + (2 * min2);
+        ribbon += x * y * z;
+    }
 
-		int z{ Arr_tools::convertSubarrayToInt(line, left, right) };
+    // answer
+    std::cout << "Total ammount of wrapping paper elf needs is equal to " << paper << " square feet\n";
+    std::cout << "and the total ammoun of ribbon elf needs is equal to " << ribbon << " feet\n";
 
-		int min1{ Math_tools::min_value(x, y, z) };
-		int min2{};
-		if (min1 == x)
-		{
-			min2 = { Math_tools::min_value(y, z) };
-		}
-		else if (min1 == y)
-		{
-			min2 = { Math_tools::min_value(x, z) };
-		}
-		else
-		{
-			min2 = { Math_tools::min_value(x, y) };
-		}
+    // closing file
+    file.close();
 
-		paper += Math_tools::cuboid_surface(x, y, z);
-		paper += min1 * min2;
+    Console_tools::wait_for_enter();
 
-		ribbon += (2 * min1) + (2 * min2);
-		ribbon += x * y * z;
-	}
-
-
-	// answer
-	std::cout << "Total ammount of wrapping paper elf needs is equal to " << paper << " square feet\n";
-	std::cout << "and the total ammoun of ribbon elf needs is equal to " << ribbon << " feet\n";
-
-	// closing file
-	file.close();
-
-	Console_tools::wait_for_enter();
-
-	return 0;
+    return 0;
 }
